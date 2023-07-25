68f19c1256ff34188c32b72737bd1421026ac89b
pasindu <pasindugura123@gmail.com>
2023-07-25 23:00:56 +0530

    I removed the file of backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,22 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
+import About from './pages/About';
+import { Menu } from '@mui/material';
+import Contact from './pages/Contact';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/> } />
+        
+        <Route path='/' element={<Home/> } />
+        <Route path='/' element={<About/> } />
+        <Route path='/' element={<Menu/> } />
+        <Route path='/' element={<Home/> } />
+        <Route path='/' element={<Contact/> }/>
+        <Route path='/' element={<Home/> } />
         </Routes>
       </BrowserRouter>
 
